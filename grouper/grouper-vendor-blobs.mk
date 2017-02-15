ef2e0a79d01a19ddc32e6c72f33179a6871d16ce
Harry Youd <harry@harryyoud.co.uk>
2017-02-15 21:32:46 +0000

    grouper: Line up the makefile with UA

--- grouper/grouper-vendor-blobs.mk
+++ grouper/grouper-vendor-blobs.mk
@@ -1,13 +1,20 @@
 VENDOR_FOLDER := vendor/nvidia/grouper
 
 PRODUCT_COPY_FILES += \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/gralloc.tegra3.so:system/vendor/lib/gralloc.tegra3.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/hwcomposer.tegra3.so:system/vendor/lib/hwcomposer.tegra3.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/firmware/nvavp_os_00001000.bin:system/vendor/firmware/nvavp_os_00001000.bin \
+	$(VENDOR_FOLDER)/proprietary/vendor/firmware/nvavp_os_0ff00000.bin:system/vendor/firmware/nvavp_os_0ff00000.bin \
+	$(VENDOR_FOLDER)/proprietary/vendor/firmware/nvavp_os_e0000000.bin:system/vendor//firmware/nvavp_os_e0000000.bin \
+	$(VENDOR_FOLDER)/proprietary/vendor/firmware/nvavp_os_eff00000.bin:system/vendor/firmware/nvavp_os_eff00000.bin \
+	$(VENDOR_FOLDER)/proprietary/vendor/firmware/nvavp_vid_ucode_alt.bin:system/vendor/firmware/nvavp_vid_ucode_alt.bin \
+	$(VENDOR_FOLDER)/proprietary/etc/nvcamera.conf:system/etc/nvcamera.conf \
+	$(VENDOR_FOLDER)/proprietary/etc/nvram.txt:system/etc/nvram.txt \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/egl/libEGL_tegra.so:system/vendor/lib/egl/libEGL_tegra.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/egl/libGLESv1_CM_tegra.so:system/vendor/lib/egl/libGLESv1_CM_tegra.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/egl/libGLESv2_tegra.so:system/vendor/lib/egl/libGLESv2_tegra.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/gralloc.tegra3.so:system/vendor/lib/hw/gralloc.tegra3.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/hwcomposer.tegra3.so:system/vendor/lib/hw/hwcomposer.tegra3.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libardrv_dynamic.so:system/vendor/lib/libardrv_dynamic.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libcgdrv.so:system/vendor/lib/libcgdrv.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libEGL_tegra.so:system/vendor/lib/egl/libEGL_tegra.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libGLESv1_CM_tegra.so:system/vendor/lib/egl/libGLESv1_CM_tegra.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libGLESv2_tegra.so:system/vendor/lib/egl/libGLESv2_tegra.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvapputil.so:system/vendor/lib/libnvapputil.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvasfparserhal.so:system/vendor/lib/libnvasfparserhal.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvaviparserhal.so:system/vendor/lib/libnvaviparserhal.so \
@@ -17,11 +24,6 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvddk_2d_v2.so:system/vendor/lib/libnvddk_2d_v2.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvdispmgr_d.so:system/vendor/lib/libnvdispmgr_d.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmm.so:system/vendor/lib/libnvmm.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite.so:system/vendor/lib/libnvmmlite.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_audio.so:system/vendor/lib/libnvmmlite_audio.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_image.so:system/vendor/lib/libnvmmlite_image.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_utils.so:system/vendor/lib/libnvmmlite_utils.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_video.so:system/vendor/lib/libnvmmlite_video.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmm_audio.so:system/vendor/lib/libnvmm_audio.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmm_camera.so:system/vendor/lib/libnvmm_camera.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmm_contentpipe.so:system/vendor/lib/libnvmm_contentpipe.so \
@@ -33,6 +35,11 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmm_utils.so:system/vendor/lib/libnvmm_utils.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmm_video.so:system/vendor/lib/libnvmm_video.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmm_writer.so:system/vendor/lib/libnvmm_writer.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite.so:system/vendor/lib/libnvmmlite.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_audio.so:system/vendor/lib/libnvmmlite_audio.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_image.so:system/vendor/lib/libnvmmlite_image.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_utils.so:system/vendor/lib/libnvmmlite_utils.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvmmlite_video.so:system/vendor/lib/libnvmmlite_video.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvodm_dtvtuner.so:system/vendor/lib/libnvodm_dtvtuner.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvodm_hdmi.so:system/vendor/lib/libnvodm_hdmi.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvodm_imager.so:system/vendor/lib/libnvodm_imager.so \
@@ -47,6 +54,4 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvsm.so:system/vendor/lib/libnvsm.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvtvmr.so:system/vendor/lib/libnvtvmr.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvwinsys.so:system/vendor/lib/libnvwinsys.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvwsi.so:system/vendor/lib/libnvwsi.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libstagefrighthw.so:system/vendor/lib/libstagefrighthw.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libtf_crypto_sst.so:system/vendor/lib/libtf_crypto_sst.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvwsi.so:system/vendor/lib/libnvwsi.so
